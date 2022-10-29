a2e32686d49503ab018efd5bdd6ebc6a1cbe1ba7
juancapp <juanitocamo3@gmail.com>
2022-10-29 19:38:27 -0300

    TG-1: delete function

---
+++ src/Components/SuperAdmins/List/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './list.module.css';
+
+const List = ({ superAdmins, deleteSuperAdmin }) => {
+  return (
+    <div className={styles.container}>
+      <table>
+        <thead>
+          <tr>
+            <th id="name">Name</th>
+            <th id="last-name">Last name</th>
+            <th id="email">Email</th>
+            <th id="dni">DNI</th>
+            <th id="phone">Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {superAdmins.map((superAdmin) => {
+            return (
+              <tr key={superAdmin._id}>
+                <td>{superAdmin.name}</td>
+                <td>{superAdmin.lastName}</td>
+                <td>{superAdmin.email}</td>
+                <td>{superAdmin.dni}</td>
+                <td>{superAdmin.phone}</td>
+                <td>
+                  <button>Edit</button>
+                  <button onClick={() => deleteSuperAdmin(superAdmin._id)}>X</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default List;
--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import styles from './super-admins.module.css';
+import List from './List';
 
 function SuperAdmins() {
   const [superAdmins, saveSuperAdmins] = useState([]);
@@ -8,24 +9,29 @@ function SuperAdmins() {
     try {
       const response = await fetch(`http://localhost:5000/super-admins`);
       const data = await response.json();
-      saveSuperAdmins(data);
+      saveSuperAdmins(data.data);
     } catch (error) {
       console.error(error);
     }
   }, []);
 
+  const deleteSuperAdmin = async (id) => {
+    await fetch(`http://localhost:5000/super-admins/${id}`, {
+      method: 'DELETE'
+    });
+    const newSuperAdmins = superAdmins.filter((superAdmin) => superAdmin._id !== id);
+    console.log(newSuperAdmins);
+    saveSuperAdmins(newSuperAdmins);
+  };
+
   return (
     <section className={styles.container}>
       <h2>SuperAdmins</h2>
-      <div>
-        {superAdmins.data?.map((superAdmin) => {
-          return (
-            <a href="/super-admins/form" key={superAdmin._id}>
-              {superAdmin.name}
-            </a>
-          );
-        })}
-      </div>
+      {superAdmins ? (
+        <List superAdmins={superAdmins} deleteSuperAdmin={deleteSuperAdmin} />
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </section>
   );
 }
