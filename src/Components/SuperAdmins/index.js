d858bae2123bc8a47b9f6a44e444a60b8eff2b08
juancapp <juanitocamo3@gmail.com>
2022-10-29 14:57:20 -0300

    TG-1: first part

--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
 import styles from './super-admins.module.css';
 
 function SuperAdmins() {
+  const [superAdmins, saveSuperAdmins] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/super-admins`);
+      const data = await response.json();
+      saveSuperAdmins(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>SuperAdmins</h2>
+      <div>
+        {superAdmins.data?.map((superAdmin) => {
+          return (
+            <a href="/super-admins/form" key={superAdmin._id}>
+              {superAdmin.name}
+            </a>
+          );
+        })}
+      </div>
     </section>
   );
 }
