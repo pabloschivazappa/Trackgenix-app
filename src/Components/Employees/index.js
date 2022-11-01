134ee725504a533efbcf618ed6e23d369047c630
mgriolfo-g <mgriolfo@gmail.com>
2022-10-31 23:51:51 -0300

    TG-10: integrated modal into main employee file (delete function)

--- src/Components/Employees/index.js
+++ src/Components/Employees/index.js
@@ -1,9 +1,25 @@
 import { useEffect, useState } from 'react';
 import styles from './employees.module.css';
 import List from './List';
+import Modal from './Modal';
 
 function Employees() {
   const [employees, saveEmployees] = useState([]);
+
+  const [modalDisplay, setModalDisplay] = useState('');
+  const [contentMessage, setContentMessage] = useState('');
+  const [modalTitle, setModalTitle] = useState('');
+
+  const deleteMessage = (contentSubTitle, name, lastName, email, password, dni, phone) => {
+    return ` ${contentSubTitle}:\n
+  Name: ${name}
+  Last Name: ${lastName}
+  Email: ${email}
+  Password: ${password}
+  Dni: ${dni}
+  Phone: ${phone}
+  `;
+  };
 
   useEffect(async () => {
     try {
@@ -14,15 +30,37 @@ function Employees() {
       console.error(error);
     }
   }, []);
-  console.log(employees);
+
   const deleteEmployee = async (id) => {
     if (confirm('¿Delete employee?')) {
-      await fetch(`${process.env.REACT_APP_API_URL}/employees/${id}`, {
-        method: 'DELETE'
-      });
-      const newEmployees = employees.filter((employee) => employee._id !== id);
-      console.log(newEmployees);
-      saveEmployees(newEmployees);
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/employees/${id}`, {
+          method: 'DELETE'
+        });
+        const newEmployees = employees.filter((employee) => employee._id !== id);
+        saveEmployees(newEmployees);
+
+        const data = await response.json();
+        setModalTitle('Delete employee');
+        if (data.error === true) {
+          setContentMessage(data.message);
+        } else {
+          setContentMessage(() =>
+            deleteMessage(
+              data.message,
+              data.data.name,
+              data.data.lastName,
+              data.data.email,
+              data.data.password,
+              data.data.dni,
+              data.data.phone
+            )
+          );
+        }
+      } catch (error) {
+        setContentMessage(error);
+      }
+      setModalDisplay(true);
     }
   };
 
@@ -34,6 +72,13 @@ function Employees() {
       ) : (
         <h2>Loading...</h2>
       )}
+      {modalDisplay ? (
+        <Modal
+          title={modalTitle}
+          contentMessage={contentMessage}
+          setModalDisplay={setModalDisplay}
+        />
+      ) : null}
     </section>
   );
 }
