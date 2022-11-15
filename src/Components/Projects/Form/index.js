9131080d8e6122ac7cb52ee87c6b209e19caf742
juancapp <juanitocamo3@gmail.com>
2022-11-15 00:09:25 -0300

    TG-49: WIP error current

--- src/Components/Projects/Form/index.js
+++ src/Components/Projects/Form/index.js
@@ -3,7 +3,11 @@ import formStyles from './form.module.css';
 import Modal from '../../Shared/Modal';
 import Form from '../../Shared/Form';
 import Input from '../../Shared/Input';
+import Spinner from '../../Shared/Spinner';
 import FunctionalButton from '../../Shared/Buttons/FunctionalButton';
+import { useSelector, useDispatch } from 'react-redux';
+import { createProject, editProject } from '../../../redux/projects/thunks';
+import { setFetching } from '../../../redux/projects/actions';
 
 const AddItem = () => {
   const urlValues = window.location.search;
@@ -11,7 +15,8 @@ const AddItem = () => {
   const id = urlParams.get('id');
   const idRegEx = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i;
   const rowId = idRegEx.test(id);
-
+  const { children, modalTitle, fetching } = useSelector((state) => state.projects);
+  const dispatch = useDispatch();
   const initialValue = {
     name: '',
     description: '',
@@ -23,144 +28,65 @@ const AddItem = () => {
   };
   const [project, setProject] = useState(initialValue);
   const [employees, setEmployees] = useState([]);
-
   const [modalDisplay, setModalDisplay] = useState('');
-  const [children, setChildren] = useState('');
-  const [modalTitle, setModalTitle] = useState('');
+  // const [children, setChildren] = useState('');
+  // const [modalTitle, setModalTitle] = useState('');
 
-  const editAndCreateMessage = (
-    contentSubTitle,
-    name,
-    description,
-    clientName,
-    startDate,
-    endDate,
-    employees,
-    active
-  ) => {
-    return ` ${contentSubTitle}:\n
-  Name: ${name}
-  Description: ${description}
-  Client Name: ${clientName}
-  Start Date: ${startDate}
-  End Date: ${endDate}
-  Employees: ${employees}
-  Active: ${active}
-  `;
-  };
+  // const editAndCreateMessage = (
+  //   contentSubTitle,
+  //   name,
+  //   description,
+  //   clientName,
+  //   startDate,
+  //   endDate,
+  //   employees,
+  //   active
+  // ) => {
+  //   return ` ${contentSubTitle}:\n
+  // Name: ${name}
+  // Description: ${description}
+  // Client Name: ${clientName}
+  // Start Date: ${startDate}
+  // End Date: ${endDate}
+  // Employees: ${employees}
+  // Active: ${active}
+  // `;
+  // };
 
   useEffect(async () => {
     if (rowId) {
+      dispatch(setFetching(true));
       try {
         const response = await fetch(`${process.env.REACT_APP_API_URL}/projects/${id}`);
         const data = await response.json();
         setProject({
           name: data.data.name,
-          clientName: data.data.clientName,
-          description: data.data.description,
-          startDate: data.data.startDate.substr(0, 10),
-          endDate: data.data.endDate.substr(0, 10),
-          active: true
+          lastName: data.data.lastName,
+          email: data.data.email,
+          password: data.data.password,
+          dni: data.data.dni,
+          phone: data.data.phone
         });
-        setEmployees(
-          data.data.employees.filter((item) => {
-            item.employee !== null;
-          })
-        );
       } catch (error) {
-        alert('Could not GET Project.', error);
+        console.error(error);
       }
-    } else {
-      return null;
+      dispatch(setFetching(false));
     }
   }, []);
 
-  const editItem = async ({ name, description, clientName, startDate, endDate, active }) => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/projects/${id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          name,
-          description,
-          clientName,
-          startDate,
-          endDate,
-          employees: employees,
-          active: active
-        })
-      });
-      const data = await response.json();
-      setModalTitle('Edit super admin');
-      if (!response.ok) {
-        setChildren(data.message);
-      } else {
-        setChildren(() =>
-          editAndCreateMessage(
-            data.message,
-            data.data.name,
-            data.data.clientName,
-            data.data.description,
-            data.data.startDate.substr(0, 10),
-            data.data.endDate.substr(0, 10),
-            `${data.data.employees[0].employee.name} ${data.data.employees[0].employee.lastName}`,
-            data.data.active
-          )
-        );
-      }
-    } catch (error) {
-      setChildren(error);
-    }
+  const addProject = () => {
+    dispatch(createProject(project));
     setModalDisplay(true);
   };
 
-  const createProject = async ({ name, description, clientName, startDate, endDate, active }) => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/projects`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          name,
-          description,
-          clientName,
-          startDate,
-          endDate,
-          employees: employees,
-          active
-        })
-      });
-      const data = await response.json();
-      setModalTitle('Create project');
-      if (!response.ok) {
-        setChildren(data.message);
-      } else {
-        setChildren(() =>
-          editAndCreateMessage(
-            data.message,
-            data.data.name,
-            data.data.clientName,
-            data.data.description,
-            data.data.startDate.substr(0, 10),
-            data.data.endDate.substr(0, 10),
-            data.data.employees,
-            data.data.active
-          )
-        );
-      }
-    } catch (error) {
-      setChildren(error);
-    }
+  const putProject = () => {
+    dispatch(editProject(rowId, project));
     setModalDisplay(true);
   };
 
   const onSubmit = (e) => {
-    if (!rowId) {
-      e.preventDefault();
-      createProject(project);
-    } else {
-      e.preventDefault();
-      editItem(project);
-    }
+    e.preventDefault();
+    !rowId ? addProject(project) : putProject(project);
   };
 
   return (
@@ -170,113 +96,119 @@ const AddItem = () => {
         buttonMessage={rowId ? 'Edit' : 'Create'}
         formTitle={rowId ? 'Edit Project' : 'Create Project'}
       >
-        <Input
-          title="Project Name"
-          name="name"
-          value={project.name}
-          onChange={(e) => setProject({ ...project, name: e.target.value })}
-        />
-        <Input
-          title="Description"
-          name="description"
-          value={project.description}
-          onChange={(e) => setProject({ ...project, description: e.target.value })}
-        />
-        <Input
-          title="Client Name"
-          name="clientName"
-          value={project.clientName}
-          onChange={(e) => setProject({ ...project, clientName: e.target.value })}
-        />
-        <Input
-          title="Start Date"
-          type="date"
-          name="startDate"
-          value={project.startDate}
-          onChange={(e) => setProject({ ...project, startDate: e.target.value })}
-        />
-        <Input
-          title="End Date"
-          type="date"
-          name="endDate"
-          value={project.endDate}
-          onChange={(e) => setProject({ ...project, endDate: e.target.value })}
-        />
-        <div className={formStyles.form__container}>
-          <label className={formStyles.form__label}> Add Employees (optional)</label>
-          {employees.map((employee, index) => (
-            <div key={index} className={formStyles.employee__form}>
-              <Input
-                name="employee"
-                title="Employee"
-                value={employees[index].employee._id}
-                onChange={(e) =>
-                  setEmployees([
-                    ...employees.slice(0, index),
-                    {
-                      ...employee,
-                      employee: e.target.value.slice(-24)
-                    },
-                    ...employees.slice(index + 1)
-                  ])
-                }
-              />
-              <Input
-                title="Rate"
-                name="rate"
-                value={employee.rate}
-                onChange={(e) =>
-                  setEmployees([
-                    ...employees.slice(0, index),
-                    {
-                      ...employee,
-                      rate: e.target.value
-                    },
-                    ...employees.slice(index + 1)
-                  ])
-                }
-              />
-              <Input
-                title="Role"
-                name="role"
-                value={employee.role}
-                onChange={(e) =>
-                  setEmployees([
-                    ...employees.slice(0, index),
-                    {
-                      ...employee,
-                      role: e.target.value
-                    },
-                    ...employees.slice(index + 1)
-                  ])
-                }
-              />
+        {!fetching ? (
+          <>
+            <Input
+              title="Project Name"
+              name="name"
+              value={project.name}
+              onChange={(e) => setProject({ ...project, name: e.target.value })}
+            />
+            <Input
+              title="Description"
+              name="description"
+              value={project.description}
+              onChange={(e) => setProject({ ...project, description: e.target.value })}
+            />
+            <Input
+              title="Client Name"
+              name="clientName"
+              value={project.clientName}
+              onChange={(e) => setProject({ ...project, clientName: e.target.value })}
+            />
+            <Input
+              title="Start Date"
+              type="date"
+              name="startDate"
+              value={project.startDate}
+              onChange={(e) => setProject({ ...project, startDate: e.target.value })}
+            />
+            <Input
+              title="End Date"
+              type="date"
+              name="endDate"
+              value={project.endDate}
+              onChange={(e) => setProject({ ...project, endDate: e.target.value })}
+            />
+            <div className={formStyles.form__container}>
+              <label className={formStyles.form__label}> Add Employees (optional)</label>
+              {employees.map((employee, index) => (
+                <div key={index} className={formStyles.employee__form}>
+                  <Input
+                    name="employee"
+                    title="Employee"
+                    value={employees[index].employee._id}
+                    onChange={(e) =>
+                      setEmployees([
+                        ...employees.slice(0, index),
+                        {
+                          ...employee,
+                          employee: e.target.value.slice(-24)
+                        },
+                        ...employees.slice(index + 1)
+                      ])
+                    }
+                  />
+                  <Input
+                    title="Rate"
+                    name="rate"
+                    value={employee.rate}
+                    onChange={(e) =>
+                      setEmployees([
+                        ...employees.slice(0, index),
+                        {
+                          ...employee,
+                          rate: e.target.value
+                        },
+                        ...employees.slice(index + 1)
+                      ])
+                    }
+                  />
+                  <Input
+                    title="Role"
+                    name="role"
+                    value={employee.role}
+                    onChange={(e) =>
+                      setEmployees([
+                        ...employees.slice(0, index),
+                        {
+                          ...employee,
+                          role: e.target.value
+                        },
+                        ...employees.slice(index + 1)
+                      ])
+                    }
+                  />
+                  <FunctionalButton
+                    title="Delete"
+                    action={(e) => {
+                      e.target.closest('div').remove();
+                    }}
+                    buttonType="form__button__add__employee"
+                    buttonColor="red"
+                  />
+                </div>
+              ))}
               <FunctionalButton
-                title="Delete"
-                action={(e) => {
-                  e.target.closest('div').remove();
-                }}
+                title="Add"
+                action={() =>
+                  setEmployees([
+                    ...employees,
+                    {
+                      employee: '',
+                      rate: 0,
+                      role: ''
+                    }
+                  ])
+                }
                 buttonType="form__button__add__employee"
-                buttonColor="red"
+                buttonColor="grayish-navy"
               />
             </div>
-          ))}
-          <FunctionalButton
-            title="Add"
-            action={() =>
-              setEmployees([
-                ...employees,
-                {
-                  employee: '',
-                  rate: 0,
-                  role: ''
-                }
-              ])
-            }
-            buttonType="form__button__add__employee"
-            buttonColor="grayish-navy"
-          />
-        </div>
+          </>
+        ) : (
+          <Spinner />
+        )}
       </Form>
       {modalDisplay ? (
         <Modal title={modalTitle} setModalDisplay={setModalDisplay}>
--- src/redux/projects/actions.js
+++ src/redux/projects/actions.js
@@ -2,11 +2,18 @@ import {
   GET_PROJECTS_PENDING,
   GET_PROJECTS_SUCCESS,
   GET_PROJECTS_ERROR,
-  DELETE_PROJECTS_ERROR,
-  DELETE_PROJECTS_SUCCESS,
   DELETE_PROJECTS_PENDING,
+  DELETE_PROJECTS_SUCCESS,
+  DELETE_PROJECTS_ERROR,
+  POST_PROJECTS_PENDING,
+  POST_PROJECTS_SUCCESS,
+  POST_PROJECTS_ERROR,
+  PUT_PROJECTS_PENDING,
+  PUT_PROJECTS_SUCCESS,
+  PUT_PROJECTS_ERROR,
   SET_MODAL_TITLE,
-  SET_MODAL_CONTENT
+  SET_MODAL_CONTENT,
+  SET_FETCHING_VALUE
 } from './constants';
 
 export const getProjectsPending = () => {
@@ -60,5 +67,52 @@ export const deleteProjectsError = (error) => {
   return {
     type: DELETE_PROJECTS_ERROR,
     payload: error ? error : 'Error'
+  };
+};
+
+export const postProjectsPending = () => {
+  return {
+    type: POST_PROJECTS_PENDING
+  };
+};
+
+export const postProjectsSuccess = (payload) => {
+  return {
+    type: POST_PROJECTS_SUCCESS,
+    payload
+  };
+};
+
+export const postProjectsError = (payload) => {
+  return {
+    type: POST_PROJECTS_ERROR,
+    payload
+  };
+};
+
+export const putProjectsPending = () => {
+  return {
+    type: PUT_PROJECTS_PENDING
+  };
+};
+
+export const putProjectsSuccess = (payload) => {
+  return {
+    type: PUT_PROJECTS_SUCCESS,
+    payload
+  };
+};
+
+export const putProjectsError = (payload) => {
+  return {
+    type: PUT_PROJECTS_ERROR,
+    payload
+  };
+};
+
+export const setFetching = (payload) => {
+  return {
+    type: SET_FETCHING_VALUE,
+    payload
   };
 };
