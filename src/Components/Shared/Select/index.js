dca31b429d515d3cdc564ac56ac69afad7b7412e
juancapp <juanitocamo3@gmail.com>
2022-11-18 12:35:19 -0300

    TG: fix style bug

--- src/Components/Shared/Select/index.js
+++ src/Components/Shared/Select/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import styles from '../Select/select.module.css';
 
-const Select = ({ input, onChange, list, name, kind, id = null }) => {
+const Select = ({ input, onChange, list, name, kind, id = null, title }) => {
   return (
-    <select name={name} value={input} onChange={onChange}>
-      {!id && <option hidden>- Select {name} -</option>}
-      <option hidden>Please select an existing {name}</option>
-      {list.map((item) => (
-        <option value={item._id} key={item._id}>
-          {item[kind]}
-        </option>
-      ))}
-    </select>
+    <label className={styles.label}>
+      {title}
+      <select name={name} value={input} onChange={onChange} className={styles.select}>
+        {!id && <option hidden>- Select {name} -</option>}
+        <option hidden>- Please select an existing {name} -</option>
+        {list.map((item) => (
+          <option value={item._id} key={item._id} className={styles.select}>
+            {item[kind]}
+          </option>
+        ))}
+      </select>
+    </label>
   );
 };
 
