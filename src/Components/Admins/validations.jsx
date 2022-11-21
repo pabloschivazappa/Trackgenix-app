1797046a74f28bf7ac96008c2edd4744fd1a74fa
mgriolfo-g <mgriolfo@gmail.com>
2022-11-21 14:12:11 -0300

    TG-61: implemented validations on admin form

---
+++ src/Components/Admins/validations.jsx
@@ -0,0 +1,70 @@
+import Joi from 'joi';
+
+export const schema = Joi.object({
+  name: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(50)
+    .pattern(/^([^0-9]*)$/i)
+    .required()
+    .messages({
+      'string.alphanum': 'Name must not contain special characters.',
+      'string.min': 'Name must be at least 3 characters long.',
+      'string.max': 'Name must be no longer than 50 characters.',
+      'string.pattern.base': 'Name must have only letters.',
+      'string.empty': 'Name must not be an empty field.',
+      'any.required': 'Name is required.'
+    }),
+  lastName: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(50)
+    .pattern(/^([^0-9]*)$/i)
+    .required()
+    .messages({
+      'string.alphanum': 'Last name must not contain special characters.',
+      'string.min': 'Last name must be at least 3 characters long.',
+      'string.max': 'Last name must be no longer than 50 characters.',
+      'string.pattern.base': 'Last name must have only letters.',
+      'string.empty': 'Last name must not be an empty field.',
+      'any.required': 'Last name is required.'
+    }),
+  phone: Joi.number().min(10000000).max(999999999999999).required().messages({
+    'number.base': 'Phone must be a number.',
+    'number.min': 'Phone must be at least 8 numbers long.',
+    'number.max': 'Phone must be no longer than 15 numbers.',
+    'number.unsafe': 'Phone must be no longer than 15 numbers.',
+    'number.empty': 'Phone must not be an empty field.',
+    'any.required': 'Phone is required.'
+  }),
+  dni: Joi.number().min(100000).max(999999999999999).required().messages({
+    'number.base': 'DNI must be a number.',
+    'number.min': 'DNI must be at least 6 numbers long.',
+    'number.max': 'DNI must be no longer than 15 numbers.',
+    'number.unsafe': 'DNI must be no longer than 15 numbers.',
+    'number.empty': 'DNI must not be an empty field.',
+    'any.required': 'DNI is required.'
+  }),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .messages({
+      'string.email': 'Invalid email.',
+      'string.empty': 'Email must not be an empty field.',
+      'any.required': 'Email is required.'
+    }),
+  password: Joi.string()
+    .alphanum()
+    .min(8)
+    .max(50)
+    .pattern(/^[a-zA-Z0-9]{8,50}$/)
+    .required()
+    .messages({
+      'string.alphanum': 'Password must not contain special characters.',
+      'string.min': 'Password must be at least 8 characters long.',
+      'string.max': 'Password must be no longer than 50 characters.',
+      'string.pattern.base': 'Password must contain letters and numbers.',
+      'string.empty': 'Password must not be an empty field.',
+      'any.required': 'Password is required.'
+    })
+});
