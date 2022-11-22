ff2c1d6fb8da476d650c2722e0ed2dba3d82cc75
juancapp <juanitocamo3@gmail.com>
2022-11-22 19:24:36 -0300

    TG-68: implement react lazy

--- src/Components/Routes/index.js
+++ src/Components/Routes/index.js
@@ -1,57 +1,58 @@
 import React from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import Header from 'Components/Header/index';
-import Footer from 'Components/Footer/index';
-import Admins from 'Components/Admins/index';
-import FormAdmins from 'Components/Admins/FormAdmins';
-import SuperAdmins from 'Components/SuperAdmins/index';
-import SuperAdminsForm from 'Components/SuperAdmins/Form';
-import Home from 'Components/Home/index';
+const Header = React.lazy(() => import('Components/Header/index'));
+const Footer = React.lazy(() => import('Components/Footer/index'));
+const Admins = React.lazy(() => import('Components/Admins/index'));
+const FormAdmins = React.lazy(() => import('Components/Admins/FormAdmins'));
+const SuperAdmins = React.lazy(() => import('Components/SuperAdmins/index'));
+const SuperAdminsForm = React.lazy(() => import('Components/SuperAdmins/Form'));
+const Home = React.lazy(() => import('Components/Home/index'));
 import styles from 'Components/Routes/layout.module.css';
-import Employees from 'Components/Employees/index';
-import EmployeesForm from 'Components/Employees/Form';
-import Projects from 'Components/Projects';
-import TimeSheets from 'Components/TimeSheets';
-import Tasks from 'Components/Tasks/index';
-import TimesheetsForm from 'Components/TimeSheets/Form/TimesheetsForm';
-import ProjectsForm from 'Components/Projects/Form/index';
-import TasksForm from 'Components/Tasks/Form';
-import ProjectTable from 'Components/ProjectsTable';
-import EmployeesProfile from 'Components/Employees/Profile';
+const Employees = React.lazy(() => import('Components/Employees/index'));
+const EmployeesForm = React.lazy(() => import('Components/Employees/Form'));
+const Projects = React.lazy(() => import('Components/Projects'));
+const TimeSheets = React.lazy(() => import('Components/TimeSheets'));
+const Tasks = React.lazy(() => import('Components/Tasks/index'));
+const TimesheetsForm = React.lazy(() => import('Components/TimeSheets/Form/TimesheetsForm'));
+const ProjectsForm = React.lazy(() => import('Components/Projects/Form/index'));
+const TasksForm = React.lazy(() => import('Components/Tasks/Form'));
+const ProjectTable = React.lazy(() => import('Components/ProjectsTable'));
+const EmployeesProfile = React.lazy(() => import('Components/Employees/Profile'));
 
 const Routes = () => {
   return (
-    <div className={styles.container}>
-      <Header />
-      <Switch>
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/admins" component={Admins} />
-        <Route path="/admins/form" component={FormAdmins} />
-        <Route path="/admins/form?id=" component={FormAdmins} />
-        <Route exact path="/employees" component={Employees} />
-        <Route path="/employees/form" component={EmployeesForm} />
-        <Route path="/employees/form?id=" component={EmployeesForm} />
-        <Route path="/employees/profile" component={EmployeesProfile} />
-        <Route exact path="/projects" component={Projects} />
-        <Route path="/projects/form" component={ProjectsForm} />
-        <Route path="/projects/form?id=" component={ProjectsForm} />
-        <Route exact path="/super-admins" component={SuperAdmins} />
-        <Route path="/super-admins/form" component={SuperAdminsForm} />
-        <Route path="/super-admins/form?id=" component={SuperAdminsForm} />
-        <Route exact path="/tasks" component={Tasks} />
-        <Route path="/tasks/form" component={TasksForm} />
-        <Route path="/tasks/form?id=" component={TasksForm} />
-        <Route exact path="/time-sheets" component={TimeSheets} />
-        <Route path="/time-sheets/form" component={TimesheetsForm} />
-        <Route path="/time-sheets/form?id=" component={TimesheetsForm} />
-        <Route exact path="/">
-          <Redirect to="/home?id=637556a4d6689c383fac4a66" />
-        </Route>
-        {/* <Route path="/employees/my-profile" component={ProjectTable} /> */}
-        <Route path="/employees/projects" component={ProjectTable} />
-      </Switch>
-      <Footer />
-    </div>
+    <React.Suspense fallback={''}>
+      <div className={styles.container}>
+        <Header />
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/admins" component={Admins} />
+          <Route path="/admins/form" component={FormAdmins} />
+          <Route path="/admins/form?id=" component={FormAdmins} />
+          <Route exact path="/employees" component={Employees} />
+          <Route path="/employees/form" component={EmployeesForm} />
+          <Route path="/employees/form?id=" component={EmployeesForm} />
+          <Route path="/employees/profile" component={EmployeesProfile} />
+          <Route exact path="/projects" component={Projects} />
+          <Route path="/projects/form" component={ProjectsForm} />
+          <Route path="/projects/form?id=" component={ProjectsForm} />
+          <Route exact path="/super-admins" component={SuperAdmins} />
+          <Route path="/super-admins/form" component={SuperAdminsForm} />
+          <Route path="/super-admins/form?id=" component={SuperAdminsForm} />
+          <Route exact path="/tasks" component={Tasks} />
+          <Route path="/tasks/form" component={TasksForm} />
+          <Route path="/tasks/form?id=" component={TasksForm} />
+          <Route exact path="/time-sheets" component={TimeSheets} />
+          <Route path="/time-sheets/form" component={TimesheetsForm} />
+          <Route path="/time-sheets/form?id=" component={TimesheetsForm} />
+          <Route exact path="/">
+            <Redirect to="/home?id=637556a4d6689c383fac4a66" />
+          </Route>
+          <Route path="/employees/projects" component={ProjectTable} />
+        </Switch>
+        <Footer />
+      </div>
+    </React.Suspense>
   );
 };
 
