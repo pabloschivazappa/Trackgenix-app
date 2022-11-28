6160834ea92b2b7a284628b5f756a371855f02b5
pabloschivazappa <pablo.schivazappa@gmail.com>
2022-11-28 18:13:11 -0300

    TG-75: endpoints public to private

--- src/Components/Routes/index.js
+++ src/Components/Routes/index.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import { tokenListener } from 'helpers/firebase';
+import React, { useEffect } from 'react';
 import Spinner from 'Components/Shared/Spinner';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 const Header = React.lazy(() => import('Components/Header/index'));
 const Footer = React.lazy(() => import('Components/Footer/index'));
 const Admins = React.lazy(() => import('Components/Admins/index'));
-const FormAdmins = React.lazy(() => import('Components/Admins/FormAdmins'));
 const SuperAdmins = React.lazy(() => import('Components/SuperAdmins/index'));
-const SuperAdminsForm = React.lazy(() => import('Components/SuperAdmins/Form'));
 const Home = React.lazy(() => import('Components/Home/index'));
 import styles from 'Components/Routes/layout.module.css';
 const Employees = React.lazy(() => import('Components/Employees/index'));
-const EmployeesForm = React.lazy(() => import('Components/Employees/Form'));
 const Projects = React.lazy(() => import('Components/Projects'));
 const TimeSheets = React.lazy(() => import('Components/TimeSheets'));
 const Tasks = React.lazy(() => import('Components/Tasks/index'));
-const TimesheetsForm = React.lazy(() => import('Components/TimeSheets/Form/TimesheetsForm'));
-const ProjectsForm = React.lazy(() => import('Components/Projects/Form/index'));
-const TasksForm = React.lazy(() => import('Components/Tasks/Form'));
 const ProjectTable = React.lazy(() => import('Components/ProjectsTable'));
-const EmployeesProfile = React.lazy(() => import('Components/Employees/Profile'));
 
 const Routes = () => {
+  useEffect(() => {
+    tokenListener();
+  }, []);
+
   return (
     <React.Suspense
       fallback={
@@ -33,27 +32,15 @@ const Routes = () => {
         <Header />
         <Switch>
           <Route exact path="/home" component={Home} />
-          <Route exact path="/admins" component={Admins} />
-          <Route path="/admins/form" component={FormAdmins} />
-          <Route path="/admins/form?id=" component={FormAdmins} />
-          <Route exact path="/employees" component={Employees} />
-          <Route path="/employees/form" component={EmployeesForm} />
-          <Route path="/employees/form?id=" component={EmployeesForm} />
-          <Route path="/employees/profile" component={EmployeesProfile} />
-          <Route exact path="/projects" component={Projects} />
-          <Route path="/projects/form" component={ProjectsForm} />
-          <Route path="/projects/form?id=" component={ProjectsForm} />
-          <Route exact path="/super-admins" component={SuperAdmins} />
-          <Route path="/super-admins/form" component={SuperAdminsForm} />
-          <Route path="/super-admins/form?id=" component={SuperAdminsForm} />
-          <Route exact path="/tasks" component={Tasks} />
-          <Route path="/tasks/form" component={TasksForm} />
-          <Route path="/tasks/form?id=" component={TasksForm} />
-          <Route exact path="/time-sheets" component={TimeSheets} />
-          <Route path="/time-sheets/form" component={TimesheetsForm} />
-          <Route path="/time-sheets/form?id=" component={TimesheetsForm} />
+          <PrivateRoute exact path="/admins" role="ADMIN" component={Admins} />
+          <PrivateRoute exact path="/employees" role="EMPLOYEE" component={Employees} />
+          <PrivateRoute exact path="/projects" component={Projects} />
+          <PrivateRoute exact path="/super-admins" role="SUPER-ADMIN" component={SuperAdmins} />
+          <PrivateRoute exact path="/tasks" component={Tasks} />
+          <PrivateRoute exact path="/time-sheets" component={TimeSheets} />
+          <Redirect to="/auth" />
           <Route exact path="/">
-            <Redirect to="/home?id=637556a4d6689c383fac4a66" />
+            <Redirect to="/home" />
           </Route>
           <Route path="/employees/projects" component={ProjectTable} />
         </Switch>
