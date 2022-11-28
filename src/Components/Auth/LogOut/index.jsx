9f05ba78e2d9336ebce668e2df9da331c103dc80
juliancit0 <juulianconti@gmail.com>
2022-11-28 20:37:01 -0300

    TG-74: delete unused 'Logout' component

--- src/Components/Auth/LogOut/index.jsx
+++
@@ -1,8 +0,0 @@
-import { RedirectButton } from 'Components/Shared';
-import React from 'react';
-
-const LogOut = () => {
-  return <RedirectButton title="Log out" path="/" />;
-};
-
-export default LogOut;
