b47bc7b91e3b0c88b7f45bf9ad8ea5769ec2d364
pabloschivazappa <pablo.schivazappa@gmail.com>
2022-12-07 02:31:36 -0300

    TG-81: modify header styles, add login and sign up on it

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,26 +1,23 @@
 import styles from 'Components/Header/header.module.css';
+import TrackgenixLogo from '../../assets/TrackGENIX-logo.png';
+import { RedirectButton } from 'Components/Shared';
+
+const urlParams = new URLSearchParams(window.location.search);
+const employeeId = urlParams.get('id');
+const idRegEx = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i;
+const rowId = idRegEx.test(employeeId);
 
 function Header() {
   return (
     <header>
-      <div className={styles.container}>
-        <div className={styles.brand}>Radium Rocket</div>
-        <div>
-          <a href={'https://www.facebook.com/radiumrocket'} target={'_blank'} rel="noreferrer">
-            <i className="fa-brands fa-facebook-f fa-lg"></i>
-          </a>
-          <a href={'https://twitter.com/radiumrocket'} target={'_blank'} rel="noreferrer">
-            <i className="fa-brands fa-twitter fa-lg"></i>
-          </a>
-          <a href={'https://www.instagram.com/radium.rocket/'} target={'_blank'} rel="noreferrer">
-            <i className="fa-brands fa-instagram fa-lg"></i>
-          </a>
-        </div>
-      </div>
       <nav className={styles.navbar}>
-        <div className={styles.appName}>
-          <a href="/" style={{ textDecoration: 'none', color: 'black' }}>
-            Track<span>GENIX</span>
+        <div>
+          <a href="/">
+            <img
+              src={TrackgenixLogo}
+              alt="TrackGENIX Logo"
+              className={styles.trackgenix_logo}
+            ></img>
           </a>
         </div>
         <ul className={styles.rutes}>
@@ -43,6 +40,27 @@ function Header() {
             <a href="/tasks">tasks</a>
           </li>
         </ul>
+        <div className={styles.sign}>
+          {rowId ? (
+            <>
+              <p>
+                <a href="employees/profile?id=637556a4d6689c383fac4a66">My Profile</a>
+              </p>
+              <p>
+                <a href="employees/projects?id=637556a4d6689c383fac4a66">Projects</a>
+              </p>
+            </>
+          ) : (
+            <>
+              <p>
+                <RedirectButton path="login" title="Login" />
+              </p>
+              <button type="button">
+                <RedirectButton path="sign-up" title="Sign up" />
+              </button>
+            </>
+          )}
+        </div>
       </nav>
     </header>
   );
